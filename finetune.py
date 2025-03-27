99488b80f158010a4a4002a1527ac5dc2519a834
virotosh <vuongthanhtung@live.com>
2025-03-27 10:21:40 +0200

    init

--- finetune.py
+++ finetune.py
@@ -2,4 +2,12 @@ from util.loadEEG import *
 
 
 
-Load_BCIC_2b_raw_data(data_path='data/BCICIV_2b')
+#Load_BCIC_2b_raw_data(data_path='data/BCICIV_2b')
+
+data_path = "data/BCIC_2b_0_38HZ/"
+for i in range(1,2):
+    all_subjects = [i]
+    all_datas = []
+    train_dataset,valid_dataset,test_dataset = get_data(i,data_path,1, is_few_EA = True, target_sample=256*4)
+
+print(len(train_dataset[0][0][0]))
