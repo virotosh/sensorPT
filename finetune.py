3f294ae3b394b625740da418e4e2d659b65e3118
virotosh <vuongthanhtung@live.com>
2025-03-27 17:03:19 +0200

    test

--- finetune.py
+++ finetune.py
@@ -196,25 +196,10 @@ class LitSensorPTCausal(pl.LightningModule):
             {'optimizer': optimizer, 'lr_scheduler': lr_dict},
         )
         
-# load configs
-# -- LOSO 
-#from utils import *
-import math
+# load data
 data_path = "data/BCIC_2b_0_38HZ/"
 
 from util.loadEEG import *
-
-
-
-#Load_BCIC_2b_raw_data(data_path='data/BCICIV_2b')
-
-#data_path = "data/BCIC_2b_0_38HZ/"
-#for i in range(1,2):
-#    all_subjects = [i]
-#    all_datas = []
-#    train_dataset,valid_dataset,test_dataset = get_data(i,data_path,1, is_few_EA = True, target_sample=256*4)
-
-#print(len(train_dataset[0][0][0]))
 
 
 for i in range(1,2):
