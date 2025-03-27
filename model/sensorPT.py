d2478e618edd76021512f8921890f06c648336fa
virotosh <vuongthanhtung@live.com>
2025-03-27 09:02:40 +0200

    init

--- model/sensorPT.py
+++ model/sensorPT.py
@@ -253,13 +253,3 @@ class sensorPT(pl.LightningModule):
             {'optimizer': optimizer, 'lr_scheduler': lr_dict},
         )
         
-
-#def seed_torch(seed=1029):
-#	random.seed(seed)
-#	np.random.seed(seed)
-#	torch.manual_seed(seed)
-#	torch.cuda.manual_seed(seed)
-#	torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
-#	torch.backends.cudnn.benchmark = False
-#	torch.backends.cudnn.deterministic = True
- 
--- pretrain.py
+++ pretrain.py
@@ -6,6 +6,16 @@ from model.sensorPT import *
 from model.configs import *
 torch.set_float32_matmul_precision("medium")
 
+
+def seed_torch(seed=1029):
+	random.seed(seed)
+	np.random.seed(seed)
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed(seed)
+	torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+	torch.backends.cudnn.benchmark = False
+	torch.backends.cudnn.deterministic = True
+ 
 seed_torch(7)
 
 
