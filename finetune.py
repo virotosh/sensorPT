3c834bf9409f89654571247ebf59b95eb25e9ec8
virotosh <vuongthanhtung@live.com>
2025-04-02 13:17:21 +0300

    linear prob

--- finetune.py
+++ finetune.py
@@ -136,13 +136,14 @@ class LitSensorPT(pl.LightningModule):
         h = z.flatten(2)
         
         h = self.linear_probe1(self.drop(h))
-        pos = create_1d_absolute_sin_cos_embedding(h.shape[1], dim=64)
+        pos = create_1d_absolute_sin_cos_embedding(h.shape[1], dim=16)
         h = h + pos.repeat((h.shape[0], 1, 1)).to(h)
         
         h = torch.cat([self.cls_token.repeat((h.shape[0], 1, 1)).to(h.device), h], dim=1)
         h = h.transpose(0,1)
         h = self.decoder(h, h)[0,:,:]
         ###
+        h = h.flatten(1)
         
         h = self.linear_probe2(h)
         
