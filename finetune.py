3c114ab208786dd1f436945f79c8cb49e8ded29b
virotosh <vuongthanhtung@live.com>
2025-04-02 13:26:45 +0300

    linear prob

--- finetune.py
+++ finetune.py
@@ -101,8 +101,8 @@ class LitSensorPT(pl.LightningModule):
         self.linear_probe1   = LinearWithConstraint(2048, 16, max_norm=1)
         self.drop            = torch.nn.Dropout(p=0.50)        
         self.decoder         = torch.nn.TransformerDecoder(
-                                    decoder_layer=torch.nn.TransformerDecoderLayer(16, 2, 16*2, activation=torch.nn.functional.gelu, batch_first=False),
-                                    num_layers=2
+                                    decoder_layer=torch.nn.TransformerDecoderLayer(16, 4, 16*4, activation=torch.nn.functional.gelu, batch_first=False),
+                                    num_layers=4
                                 )
         self.cls_token =        torch.nn.Parameter(torch.rand(1,1,16)*0.001, requires_grad=True)
         self.linear_probe2   =   LinearWithConstraint(16*16, self.num_class, max_norm=0.25)
@@ -143,7 +143,7 @@ class LitSensorPT(pl.LightningModule):
         h = h.transpose(0,1)
         h = self.decoder(h, h)[0,:,:]
         ###
-        
+        h = h.flatten(1)
         h = self.linear_probe2(h)
         
         return x, h
