606a64459adf003857574d50e92f0843db8f8d91
virotosh <vuongthanhtung@live.com>
2025-04-02 12:59:21 +0300

    linear prob

--- finetune.py
+++ finetune.py
@@ -98,14 +98,14 @@ class LitSensorPT(pl.LightningModule):
         
 
         
-        self.linear_probe1   = LinearWithConstraint(2048, 16, max_norm=1)
+        self.linear_probe1   = LinearWithConstraint(2048, 64, max_norm=1)
         self.drop            = torch.nn.Dropout(p=0.50)        
         self.decoder         = torch.nn.TransformerDecoder(
-                                    decoder_layer=torch.nn.TransformerDecoderLayer(16, 4, 16*4, activation=torch.nn.functional.gelu, batch_first=False),
+                                    decoder_layer=torch.nn.TransformerDecoderLayer(64, 4, 64*4, activation=torch.nn.functional.gelu, batch_first=False),
                                     num_layers=4
                                 )
-        self.cls_token =        torch.nn.Parameter(torch.rand(1,1,16)*0.001, requires_grad=True)
-        self.linear_probe2   =   LinearWithConstraint(16*16, self.num_class, max_norm=0.25)
+        self.cls_token =        torch.nn.Parameter(torch.rand(1,1,64)*0.001, requires_grad=True)
+        self.linear_probe2   =   LinearWithConstraint(64, self.num_class, max_norm=0.25)
         
         ###
         
@@ -136,7 +136,7 @@ class LitSensorPT(pl.LightningModule):
         h = z.flatten(2)
         
         h = self.linear_probe1(self.drop(h))
-        pos = create_1d_absolute_sin_cos_embedding(h.shape[1], dim=16)
+        pos = create_1d_absolute_sin_cos_embedding(h.shape[1], dim=64)
         h = h + pos.repeat((h.shape[0], 1, 1)).to(h)
         
         h = torch.cat([self.cls_token.repeat((h.shape[0], 1, 1)).to(h.device), h], dim=1)
