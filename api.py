ba928c13cb2efc59fe13014e3939e2d28efddb7c
virotosh <vuongthanhtung@live.com>
2025-04-05 11:13:36 +0300

    checkpoint linear prob

--- api.py
+++ api.py
@@ -25,8 +25,7 @@ if __name__=="__main__":
         model = SensorPTService()
         #model.tuned_model.to(torch.device('cpu'))
         model.tuned_model.eval()
-        test_dataset.x.to(torch.device('cuda'))
-        test_dataset.y.to(torch.device('cuda'))
+        test_dataset.to(torch.device('cuda'))
         
         # predict
         _, logit = model.tuned_model(test_dataset.x)
