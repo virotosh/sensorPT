ccec1bddee29cfe70b509b39d1f138a7d17a5912
virotosh <vuongthanhtung@live.com>
2025-04-05 15:11:45 +0300

    checkpoint linear prob

--- api.py
+++ api.py
@@ -11,23 +11,15 @@ if __name__=="__main__":
         all_subjects = [i]
         all_datas = []
         train_dataset,valid_dataset,test_dataset = get_data(i,data_path,1, is_few_EA = True, target_sample=256*4)
-        
-        global max_epochs
-        global steps_per_epoch
-        global max_lr
-    
-        batch_size=64
-    
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=0, shuffle=True)
-        valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=batch_size, num_workers=0, shuffle=False)
     
         # init model
-        model = SensorPTService()
-        model.tuned_model.to(torch.device('cpu'))
-        model.tuned_model.eval()
+        service = SensorPTService()
+        model = service.tuned_model
+        model.to(torch.device('cpu'))
+        model.eval()
         print('predict')
         # predict
-        _, logit = model.tuned_model(test_dataset.x[:1])
+        _, logit = model(test_dataset.x[:1])
         #print('Y hat',torch.argmax(logit,  dim=-1))
         # accuracy
         y = test_dataset.y[:1]
