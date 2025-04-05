95df3a40b066346b1937d82c173b65a85fb57cf4
virotosh <vuongthanhtung@live.com>
2025-04-05 15:51:26 +0300

    checkpoint linear prob

--- api.py
+++ api.py
@@ -20,7 +20,7 @@ if __name__=="__main__":
         all_subjects = [i]
         all_datas = []
         train_dataset,valid_dataset,test_dataset = get_data(i,data_path,1, is_few_EA = True, target_sample=256*4)
-        print(test_dataset.x[:1].numpy())
+        print(repr(test_dataset.x[:1].numpy()))
         print('predict')
         # predict
         _, logit = model(test_dataset.x[:1])
