eba685a9898660200e922724bea75cd7bbeb77b5
virotosh <vuongthanhtung@live.com>
2025-04-07 09:09:41 +0300

    checkpoint linear prob

--- api.py
+++ api.py
@@ -1054,7 +1054,7 @@ if __name__=="__main__":
     import uvicorn
 
     uvicorn.run(
-        "main:app",
+        "api:app",
         host="0.0.0.0",
         port=8003,
         log_level="info",
