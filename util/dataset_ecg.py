4d009ca95f4ab7414efa0a291889aca2165f3840
virotosh <vuongthanhtung@live.com>
2025-04-14 19:08:59 +0300

    test

--- util/dataset_ecg.py
+++ util/dataset_ecg.py
@@ -19,29 +19,28 @@ if __name__=="__main__":
             continue
         df = pd.read_csv(CLASpath+sub+'_c_i_answers.csv')
         for ind,row in df.iterrows():
-            if (row[1]!='empty'):
-                stimulus = row['stimulus'].lower().replace('.','-')
-                
-                for i,fn in enumerate(onlyfiles):
-                    if (fn.split('_')[-1].replace('.csv','').lower() == stimulus) and 'ecg' in fn:
-                        ecg = pd.read_csv('../data/CLAS_Database/CLAS/Participants/'+sub+'/all_separate/'+fn)
-                        if(len(list(ecg['ecg2']))>512):
-                            dst="../data/merged_ecg/"
-                            if random.random()<0.1:
-                                dst+="ValidFolder/0/"
-                            else:
-                                dst+="TrainFolder/0/"
-                            os.makedirs(dst, exist_ok=True)
-                            #print(len(list(ecg['ecg2'])), fn)
-                            ecgfiles.append(fn)
-                            __tmp = []
-                            for iii in range (29):
-                                __tmp+= [list(ecg['ecg1'])[:512]*2,list(ecg['ecg2'])[:512]*2]
-                            data.append(__tmp)
-                            _d = torch.from_numpy(np.array(__tmp, dtype='float32') )
-                            labels.append(1 if row[1]=='correct' else 0)
-                            torch.save(_d, dst + 'CLAS'+f"_{sub}_{i}_{ind}.edf")
-                        break
+            stimulus = row['stimulus'].lower().replace('.','-')
+            
+            for i,fn in enumerate(onlyfiles):
+                if (fn.split('_')[-1].replace('.csv','').lower() == stimulus) and 'ecg' in fn:
+                    ecg = pd.read_csv('../data/CLAS_Database/CLAS/Participants/'+sub+'/all_separate/'+fn)
+                    if(len(list(ecg['ecg2']))>512):
+                        dst="../data/merged_ecg/"
+                        if random.random()<0.1:
+                            dst+="ValidFolder/0/"
+                        else:
+                            dst+="TrainFolder/0/"
+                        os.makedirs(dst, exist_ok=True)
+                        #print(len(list(ecg['ecg2'])), fn)
+                        ecgfiles.append(fn)
+                        __tmp = []
+                        for iii in range (29):
+                            __tmp+= [list(ecg['ecg1'])[:512]*2,list(ecg['ecg2'])[:512]*2]
+                        data.append(__tmp)
+                        _d = torch.from_numpy(np.array(__tmp, dtype='float32') )
+                        labels.append(1 if row[1]=='correct' else 0)
+                        torch.save(_d, dst + 'CLAS'+f"_{sub}_{i}_{ind}.edf")
+                    break
 
     ## for finetune 
     subs = ['Part'+str(ii) for ii in range(51,56)]
@@ -53,18 +52,17 @@ if __name__=="__main__":
             continue
         df = pd.read_csv(CLASpath+sub+'_c_i_answers.csv')
         for ind,row in df.iterrows():
-            if (row[1]!='empty'):
-                stimulus = row['stimulus'].lower().replace('.','-')
-                for i,fn in enumerate(onlyfiles):
-                    if (fn.split('_')[-1].replace('.csv','').lower() == stimulus) and 'ecg' in fn:
-                        ecg = pd.read_csv('../data/CLAS_Database/CLAS/Participants/'+sub+'/all_separate/'+fn)
-                        if(len(list(ecg['ecg2']))>512):
-                            __tmp = []
-                            for iii in range (29):
-                                __tmp+= [list(ecg['ecg1'])[:512]*2,list(ecg['ecg2'])[:512]*2]
-                            data.append(__tmp)
-                            labels.append(1 if row[1]=='correct' else 0)
-                        break
+            stimulus = row['stimulus'].lower().replace('.','-')
+            for i,fn in enumerate(onlyfiles):
+                if (fn.split('_')[-1].replace('.csv','').lower() == stimulus) and 'ecg' in fn:
+                    ecg = pd.read_csv('../data/CLAS_Database/CLAS/Participants/'+sub+'/all_separate/'+fn)
+                    if(len(list(ecg['ecg2']))>512):
+                        __tmp = []
+                        for iii in range (29):
+                            __tmp+= [list(ecg['ecg1'])[:512]*2,list(ecg['ecg2'])[:512]*2]
+                        data.append(__tmp)
+                        labels.append(1 if row[1]=='correct' else 0)
+                    break
         os.makedirs(f"../data/ECGtest/sub{sub_id}_train", exist_ok=True)
         savemat(f"../data/ECGtest/sub{sub_id}_train/Data.mat", {'x_data':data,'y_data':[labels]}, oned_as='row')
 
@@ -78,17 +76,16 @@ if __name__=="__main__":
             continue
         df = pd.read_csv(CLASpath+sub+'_c_i_answers.csv')
         for ind,row in df.iterrows():
-            if (row[1]!='empty'):
-                stimulus = row['stimulus'].lower().replace('.','-')
-                for i,fn in enumerate(onlyfiles):
-                    if (fn.split('_')[-1].replace('.csv','').lower() == stimulus) and 'ecg' in fn:
-                        ecg = pd.read_csv('../data/CLAS_Database/CLAS/Participants/'+sub+'/all_separate/'+fn)
-                        if(len(list(ecg['ecg2']))>512):
-                            __tmp = []
-                            for iii in range (29):
-                                __tmp+= [list(ecg['ecg1'])[:512]*2,list(ecg['ecg2'])[:512]*2]
-                            data.append(__tmp)
-                            labels.append(1 if row[1]=='correct' else 0)
-                        break
+            stimulus = row['stimulus'].lower().replace('.','-')
+            for i,fn in enumerate(onlyfiles):
+                if (fn.split('_')[-1].replace('.csv','').lower() == stimulus) and 'ecg' in fn:
+                    ecg = pd.read_csv('../data/CLAS_Database/CLAS/Participants/'+sub+'/all_separate/'+fn)
+                    if(len(list(ecg['ecg2']))>512):
+                        __tmp = []
+                        for iii in range (29):
+                            __tmp+= [list(ecg['ecg1'])[:512]*2,list(ecg['ecg2'])[:512]*2]
+                        data.append(__tmp)
+                        labels.append(1 if row[1]=='correct' else 0)
+                    break
         os.makedirs(f"../data/ECGtest/sub{sub_id}_test", exist_ok=True)
         savemat(f"../data/ECGtest/sub{sub_id}_test/Data.mat", {'x_data':data,'y_data':[labels]}, oned_as='row')
