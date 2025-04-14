d0ea1d4be184ad1400e7bf740c5a4babc2dab639
virotosh <vuongthanhtung@live.com>
2025-04-14 16:05:23 +0300

    test

--- util/dataset_ecg.py
+++ util/dataset_ecg.py
@@ -65,6 +65,7 @@ if __name__=="__main__":
                             data.append(__tmp)
                             labels.append(1 if row[1]=='correct' else 0)
                         break
+        os.makedirs(f"../data/ECGtest/sub{sub_id}_train/Data.mat", exist_ok=True)
         savemat(f"../data/ECGtest/sub{sub_id}_train/Data.mat", {'x_data':data,'y_data':[labels]}, oned_as='row')
 
     ## for test 
@@ -89,5 +90,5 @@ if __name__=="__main__":
                             data.append(__tmp)
                             labels.append(1 if row[1]=='correct' else 0)
                         break
-    
+        os.makedirs(f"../data/ECGtest/sub{sub_id}_test/Data.mat", exist_ok=True)
         savemat(f"../data/ECGtest/sub{sub_id}_test/Data.mat", {'x_data':data,'y_data':[labels]}, oned_as='row')
