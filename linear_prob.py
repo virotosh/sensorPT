36ee95699dbaf592c62cc84a9c55788e76617314
virotosh <vuongthanhtung@live.com>
2025-04-16 15:05:38 +0300

    test

--- linear_prob.py
+++ linear_prob.py
@@ -61,7 +61,7 @@ class LitSensorPT(pl.LightningModule):
         
         # -- load checkpoint
         #load_path="./logs/sensorPT_large_D_tb/version_0/checkpoints/epoch=199-step=51600.ckpt"
-        load_path="./logs/sensorPT_nemo_tb/version_11/checkpoints/epoch=199-step=5600.ckpt"
+        load_path="./logs/sensorPT_nemo_tb/version_1/checkpoints/epoch=199-step=5600.ckpt"
         pretrain_ckpt = torch.load(load_path, weights_only=False, map_location=torch.device("cpu"))
         
         target_encoder_stat = {}
