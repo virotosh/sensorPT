2ee4d505da030528f13bfec831f20840501e0a07
virotosh <vuongthanhtung@live.com>
2025-04-16 17:31:32 +0300

    test

--- linear_prob.py
+++ linear_prob.py
@@ -42,7 +42,7 @@ class LitSensorPT(pl.LightningModule):
 
         # init model
         target_encoder = SensorTransformerEncoder(
-            img_size=[len(use_channels_names), 256*2],
+            img_size=[len(use_channels_names), 256*4],
             patch_size=64,
             embed_num=4,
             embed_dim=512,
@@ -72,7 +72,7 @@ class LitSensorPT(pl.LightningModule):
                 
         self.target_encoder.load_state_dict(target_encoder_stat)
 
-        self.chan_conv       = Conv1dWithConstraint(48, self.chans_num, 1, max_norm=1)
+        self.chan_conv       = Conv1dWithConstraint(self.chans_num, self.chans_num, 1, max_norm=1)
         
         self.linear_probe1   =   LinearWithConstraint(2048, 16, max_norm=1)
         self.linear_probe2   =   LinearWithConstraint(16*16, self.num_class, max_norm=0.25)
