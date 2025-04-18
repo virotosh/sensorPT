361caa885adeccbede2bee744df6e63651b96920
virotosh <vuongthanhtung@live.com>
2025-04-18 10:03:26 +0300

    test

--- linear_prob.py
+++ linear_prob.py
@@ -74,7 +74,7 @@ class LitSensorPT(pl.LightningModule):
 
         self.chan_conv       = Conv1dWithConstraint(self.chans_num, self.chans_num, 1, max_norm=1)
         
-        self.linear_probe1   =   LinearWithConstraint(1024, 8, max_norm=1)
+        self.linear_probe1   =   LinearWithConstraint(2048, 8, max_norm=1)
         self.linear_probe2   =   LinearWithConstraint(8*8, self.num_class, max_norm=0.25)
         
         self.drop           = torch.nn.Dropout(p=0.20)
