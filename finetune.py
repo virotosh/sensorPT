018abafa2b2c563743e93ae1e33f0510d1e9668e
virotosh <vuongthanhtung@live.com>
2025-04-20 13:42:46 +0300

    test

--- finetune.py
+++ finetune.py
@@ -206,12 +206,12 @@ if __name__=="__main__":
         global max_lr
         print(train_dataset.y)
         print(valid_dataset.y)
-        batch_size=64
+        batch_size=8
         
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=0, shuffle=True)
         valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=batch_size, num_workers=0, shuffle=False)
         
-        max_epochs = 1
+        max_epochs = 5
         steps_per_epoch = math.ceil(len(train_loader) )
         max_lr = 0.001
     
