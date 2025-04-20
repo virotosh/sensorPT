90d2f0df46a3bde4ec8f66b2270bad480ec24e5b
virotosh <vuongthanhtung@live.com>
2025-04-20 09:32:00 +0300

    test

--- finetune.py
+++ finetune.py
@@ -75,7 +75,7 @@ class LitSensorPT(pl.LightningModule):
         self.chan_conv       = Conv1dWithConstraint(self.chans_num, self.chans_num, 1, max_norm=1)
         
         self.linear_probe1   = LinearWithConstraint(2048, 64, max_norm=1)
-        self.drop            = torch.nn.Dropout(p=0.7)        
+        self.drop            = torch.nn.Dropout(p=0.86)        
         self.decoder         = torch.nn.TransformerDecoder(
                                     decoder_layer=torch.nn.TransformerDecoderLayer(64, 4, 64*4, activation=torch.nn.functional.gelu, batch_first=False),
                                     num_layers=4
