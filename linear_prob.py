b01804c329ccd0b8f279b347e47451218ed7cedb
virotosh <vuongthanhtung@live.com>
2025-04-23 16:16:37 +0300

    one user test

--- linear_prob.py
+++ linear_prob.py
@@ -135,7 +135,7 @@ class LitSensorPT(pl.LightningModule):
         y_score = torch.cat(y_score, dim=0)
         print(label.shape, y_score.shape)
         
-        metrics = ["accuracy", "balanced_accuracy", "precision", "recall", "cohen_kappa", "f1", "roc_auc"]
+        metrics = ["accuracy", "balanced_accuracy", "cohen_kappa", "f1_weighted", "f1_macro", "f1_micro"]
         results = get_metrics(y_score.cpu().numpy(), label.cpu().numpy(), metrics, True)
         
         for key, value in results.items():
