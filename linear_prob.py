ab2aaeb999d77bd3ec903ba1c6779c5cb6ea024b
virotosh <vuongthanhtung@live.com>
2025-04-24 11:17:59 +0300

    one user test

--- linear_prob.py
+++ linear_prob.py
@@ -243,4 +243,4 @@ if __name__=="__main__":
             print('accuracy',accuracy)
             ACCURACY = np.append(ACCURACY,accuracy)
             print('AVERAGE accuracy',np.mean(ACCURACY))
-        
+        break
