0218c8719574dbd1d1ca15d306192a98e92b9c44
virotosh <vuongthanhtung@live.com>
2025-04-24 08:50:45 +0300

    one user test

--- linear_prob.py
+++ linear_prob.py
@@ -208,7 +208,7 @@ if __name__=="__main__":
         
         max_epochs = 1
         steps_per_epoch = math.ceil(len(train_loader) )
-        max_lr = 0.009
+        max_lr = 0.1
     
         # init model
         model = LitSensorPT()
